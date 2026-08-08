f14fa9630ed4adc7ad9dc16b6cf80aaf4bc82885
rt18_formula1 <rb18vcarb@gmail.com>
2026-08-08 20:24:29 +0900

    Fix Vercel Analytics setup for Vite

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
 
-createRoot(document.getElementById("root")!).render(
-  <>
-    <App />
-    <Analytics />
-  </>
-);
+createRoot(document.getElementById("root")!).render(<App />);
+
+inject();
